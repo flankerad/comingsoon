b5ec1b9d86e25091c9dbead70bb6afceaec15f64
flankerad <flanker.ad@gmail.com>
2013-09-03 12:49:45 +0530

    gh-pages files added

---
+++ process.php
@@ -0,0 +1,10 @@
+
+<style>
+	#label, #email, #submit { display: none;}
+	.text { color:#36B930}
+</style>
+<span class="text">Thank You!!<br>
+for your interest, looking forward to launch keep tight &amp; fasten your seatbelts.</span>
+
+
+
